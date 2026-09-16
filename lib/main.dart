38857467ce40dc04203302241bebd4bcbb29dd18
PV Dong <dongphung360@gmail.com>
2026-09-17 01:51:29 +0700

    hoan thanh

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,41 @@
 import 'package:flutter/material.dart';
+
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-home: Scaffold(
-body: Center(
-child: Container(
-width: 200,
-height: 200,
-decoration: BoxDecoration(
-color: Colors.yellow, // màu nền
-border: Border.all(
-color: Colors.red, // màu viền
-width: 3, // độ dày viền
-),
-shape: BoxShape.circle, // hình tròn
-),
-alignment: Alignment.center, // căn chữ vào giữa
-child: const Text(
-"Hello World",
-textAlign: TextAlign.center,
-style: TextStyle(
-fontSize: 20,
-fontWeight: FontWeight.bold,
-color: Colors.red,
-),
-),
-),
-),
-),
-);
-}
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.yellow, // Màu nền
+              border: Border.all(
+                color: Colors.red, // Màu viền
+                width: 3, // Độ dày viền
+              ),
+              shape: BoxShape.circle, // Hình tròn
+            ),
+            alignment: Alignment.center, // Căn chữ vào giữa
+            child: const Text(
+              "Hello World",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
