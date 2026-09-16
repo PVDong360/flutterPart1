c5524577636f1ccc15138bc8df72ebc73dac33fa
PV Dong <dongphung360@gmail.com>
2026-09-17 02:05:29 +0700

    hoan thanh bai b

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Icon(
-            Icons.favorite, // Loại icon
-            color: Colors.red, // Màu
-            size: 80, // Kích thước
-          ),
+        appBar: AppBar(
+          title: const Text("Icon Demo"),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            // 1. Icon mặc định
+            Icon(
+              Icons.home,
+              color: Colors.blue,
+              size: 40,
+            ),
+
+            // 2. Icon màu đỏ
+            Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 40,
+              semanticLabel: "Yêu thích",
+            ),
+
+            // 3. Icon màu xanh lá
+            Icon(
+              Icons.star,
+              color: Colors.green,
+              size: 40,
+            ),
+
+            // 4. Icon màu tím
+            Icon(
+              Icons.settings,
+              color: Colors.purple,
+              size: 40,
+            ),
+
+            // 5. Icon màu cam, lớn hơn
+            Icon(
+              Icons.alarm,
+              color: Colors.orange,
+              size: 50,
+            ),
+          ],
         ),
       ),
     );
