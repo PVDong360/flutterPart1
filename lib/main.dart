3d0f4bc4f0ffc7f296b816069961ac69cbf5b10c
PV Dong <dongphung360@gmail.com>
2026-09-17 01:34:27 +0700

    bài 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-
+void main() {
+runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context){
-        return MaterialApp(
-            title:"Welcum Stranger",
-        home: Scaffold(
-            appBar: AppBar(
-                title:Text("welcome home Stran jah"),
-            ),
-        body: Center(
-            child: Text("WELCOME HOME STRANGER",
-            style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-                backgroundColor: Colors.lightGreen,
-            ),
-            ),
-        ),
-        )
-        );
-    }
+const MyApp({super.key});
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+home: Scaffold(
+body: Center(
+child: Container(
+width: 200,
+height: 200,
+decoration: BoxDecoration(
+color: Colors.yellow, // màu nền
+border: Border.all(
+color: Colors.red, // màu viền
+width: 3, // độ dày viền
+),
+shape: BoxShape.circle, // hình tròn
+),
+alignment: Alignment.center, // căn chữ vào giữa
+child: const Text(
+"Hello World",
+textAlign: TextAlign.center,
+style: TextStyle(
+fontSize: 20,
+fontWeight: FontWeight.bold,
+color: Colors.red,
+),
+),
+),
+),
+),
+);
+}
 }
